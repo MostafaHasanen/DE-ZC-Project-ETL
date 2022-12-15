e81df082cbd4c1bbb8230c01d6ccc6adf981e84e
Mostafa Hasanen <101864501+MostafaHasanen@users.noreply.github.com>
2022-12-15 06:19:43 +0200

    Delete Docker_SQL_Week_1 directory

--- Docker_SQL_Week_1/upload_look_up.py
+++
@@ -1,8 +0,0 @@
-import pandas as pd
-df = pd.read_csv('taxi+_zone_lookup.csv')
-
-from sqlalchemy import create_engine
-engine = create_engine('postgresql://root:root@localhost:5432/ny_taxi')
-
-df.to_sql(name='zones',con=engine,if_exists='replace')
-print("Done uploading Zones")
